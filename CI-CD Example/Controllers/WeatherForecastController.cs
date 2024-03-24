ceb978ed6f3f92ab57c2c7136d2a89643ee37409
Malion Sara <sara.malion@yahoo.com>
2024-03-24 20:21:21 +0100

    Git Malion

--- CI-CD Example/Controllers/WeatherForecastController.cs
+++ CI-CD Example/Controllers/WeatherForecastController.cs
@@ -25,22 +25,10 @@ namespace CI_CD_Example.Controllers
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
-
-
-        [HttpGet(Name = "GetMalion")]
-        public IEnumerable<WeatherForecast> GetMalion()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = "Malion"
             })
             .ToArray();
         }
+
     }
 }
